b66e476d8569e10a489a4364c502f85c06eb61a3
thisita <thisita@outlook.com>
2014-03-19 03:15:26 -0400

    Impl. of MainMenuController

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenuController : MonoBehaviour {
+public class MainMenuController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Skin to use for menu controls
+    public GUISkin skin;
+
+    // Handle pausing
+    private bool _paused = false;
+    public bool Paused
+    {
+        get
+        {
+            return _paused;
+        }
+        set
+        {
+            if (value != _paused)
+            {
+                Time.timeScale = value ? 0.0f : 1.0f;
+                _paused = value;
+            }
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+    {
+	    // Handle mobile application back button event
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Exit the application
+            Application.Quit();
+        }
 	}
+
+    // Handle pause event
+    void OnApplicationPause(bool pauseStatus)
+    {
+        Paused = pauseStatus;
+    }
+
+    // Draw the menu
+    void OnGUI()
+    {
+        // use the custom skin
+        GUI.skin = skin;
+    }
 }
